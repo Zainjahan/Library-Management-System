a53664d14f203ba2298fbdbe5103512f1ea669bf
zain jahan <53372994+Zainjahan@users.noreply.github.com>
2021-07-27 12:10:57 -0700

    Delete Ecommerce Website directory

--- Ecommerce Website/ASP.NET/Controllers/ApplicationDbContext.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ASP.NET.Controllers
-{
-    internal class ApplicationDbContext
-    {
-    }
-}
--- Ecommerce Website/Ecommerce Website.sql
+++
@@ -1,5 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [Id]
-      ,[Name]
-      ,[DisplayOrder]
-  FROM [Category].[dbo].[Category]
