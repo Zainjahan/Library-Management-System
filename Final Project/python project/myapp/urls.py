0ded42396728f634823216f647e0c81df6bbf022
zain jahan <53372994+Zainjahan@users.noreply.github.com>
2021-07-27 12:11:11 -0700

    Delete Final Project/python project directory

--- Final Project/python project/myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name = 'index'),
-    path('form', views.form, name = 'form'),
-    path('submit', views.submit, name = 'submit'),
-    path('formSubmit', views.formSubmit, name= 'formSubmit')
-]
